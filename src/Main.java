3afc73ca13deb5bdd943ecbd2b803671c7606b3d
Roman <filinroman79@gmail.com>
2024-11-18 01:45:28 +0300

    HW6.1br

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String firstName = "Ivan";
+        String middleName = " Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println( "ФИО сотрудника - " + fullName);
+
+        String upperCaseFullName = fullName.toUpperCase();
+        System.out.println("Данные сотрудника для заполнения отчета -" + upperCaseFullName);
+
+        fullName = "Иванов Семён Семёнович";
+        String replecedFullName = fullName.replace('ё','е');
+        System.out.println("Данные ФИО сотрудника -" + replecedFullName);
     }
 }
